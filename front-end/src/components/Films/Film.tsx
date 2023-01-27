1640696429e2278a485ecc505d8a62832b20e170
luskoeqk <lusy2017@gmail.com>
2023-01-27 16:31:13 +0200

    Added films component, to be configured

--- front-end/src/components/Films/Film.tsx
+++ front-end/src/components/Films/Film.tsx
@@ -1,5 +1,9 @@
 
-import '../../styles/Films_styles/Films.scss';
+import { useState } from 'react';
+
+import '../../styles/Films_styles/Film.scss';
+import styled from 'styled-components';
+
 
 interface IFilm {
     title: string;
@@ -7,19 +11,69 @@ interface IFilm {
     cast: string;
     genre: string;
     year: string;
+    movie_image: string;
 }
 
 
-function Film({ title, director, cast, genre, year }: IFilm) {
+const MovieImage = styled.img`
+    width: 100%;
+    transition: filter 0.2s ease-in-out;
+
+    :hover{
+        hue-rotate(90deg);
+    }
+
+`
+
+
+
+
+function Film({ title, director, cast, genre, year, movie_image }: IFilm) {
+
+    const [isHovered, setIsHovered] = useState(false);
 
 
     return (
         <div className='FilmDivWrapper'>
-            <h2>{title}</h2>
-            <p>{director}</p>
+
+            <div className='FilmTextContainer'>
+                <div className='FilmTextContainerTitle'>
+                    <h2>{title}</h2>
+                </div>
+                <div className='FilmTextContainerYear'>
+                    <p>{year}</p>
+                </div>
+            </div>
+
+            {/* <p>{director}</p>
             <p>{cast}</p>
-            <p>{genre}</p>
-            <p>{year}</p>
+            <p>{genre}</p> */}
+
+            <div
+                className='FilmImage'
+                onMouseOver={() => setIsHovered(true)}
+                onMouseOut={() => setIsHovered(false)}
+            >
+
+                <MovieImage
+                    src={movie_image}
+                />
+                {
+                    isHovered &&
+                    <div className={`FilmImageText  ${isHovered ? 'show-text' : ''}`} >
+                        <h2>{title}</h2>
+                        <p>
+                            директор: {director}
+                            <br />
+                            <br />
+                            с участието на: {cast}
+                            <br />
+                            <br />
+                            жанр: {genre}
+                        </p>
+                    </div>
+                }
+            </div>
         </div>
     )
 }
--- front-end/src/components/Films/FilmsList.tsx
+++ front-end/src/components/Films/FilmsList.tsx
@@ -2,13 +2,15 @@
 import '../../styles/Films_styles/FilmsList.scss';
 import Film from './Film';
 
+import movie_image from '../../assets/moviehero.png';
 
 function FilmsList() {
 
 
     return (
         <div className='FilmsListWrapper'>
-            <Film title={'не поглеждай нагоре'} director={'asd'} cast={'asd'} genre={'asd'} year={'2020'}/>
+            <Film title={'не поглеждай нагоре'} director={'Кристофър Нолан'} cast={'Крисчън Бейл, Морган Фрийман, Алекса Деми'} genre={'научна фантастика'} year={'2020'} movie_image={movie_image}/>
+            <Film title={'не поглеждай нагоре'} director={'Кристофър Нолан'} cast={'Крисчън Бейл, Морган Фрийман, Алекса Деми'} genre={'научна фантастика'} year={'2020'} movie_image={movie_image}/>
         </div>
     )
 }
