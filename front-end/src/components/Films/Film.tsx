0e4510d74ff607b7578fa106857e5c4b24fdb3c1
luskoeqk <lusy2017@gmail.com>
2023-03-08 12:59:27 +0200

    film preview edit

--- front-end/src/components/Films/Film.tsx
+++ front-end/src/components/Films/Film.tsx
@@ -1,6 +1,3 @@
-
-import { useState } from 'react';
-
 import '../../styles/Films_styles/Film.scss';
 import styled from 'styled-components';
 
@@ -15,67 +12,133 @@ interface IFilm {
 }
 
 
-const MovieImage = styled.img`
-    width: 100%;
-    transition: filter 0.2s ease-in-out;
+const HoverFigure = styled.figure`
 
-    :hover{
-        hue-rotate(90deg);
+    display: grid;
+    border-radius: 1rem;
+    overflow: hidden;
+    cursor: pointer;
+
+    img {
+        width: 70vw;
     }
 
+    & > * {
+        grid-area: 1/1;
+        transition: .4s;
+    }
+
+    figcaption {
+        display: grid;
+        align-items: end;
+        font-family: sans-serif;
+        font-size: 2.3rem;
+        font-weight: bold;
+        color: #0000;
+        padding: .75rem;
+        background: var(--c,#0009);
+        clip-path: inset(0 var(--_i,100%) 0 0);
+        -webkit-mask:
+        linear-gradient(#000 0 0),
+        linear-gradient(#000 0 0);
+        -webkit-mask-composite: xor;
+        -webkit-mask-clip: text, padding-box;
+    }
+
+    &:hover figcaption{
+        --_i: 0%;
+    }
+
+    &:hover img {
+        transform: scale(1.2);
+    }
+
+    @supports not (-webkit-mask-clip: text) {
+        figcaption {
+            -webkit-mask: none;
+            color: #e4e4e4;
+        }
+    }  
+`
+
+const StyledH4 = styled.h4`
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 70px;
+`
+
+const StyledPar = styled.p`
+    font-size: 26px;
+    padding: 0px;
 `
 
 
-
-
-function Film({ title, director, cast, genre, year, movie_image }: IFilm) {
-
-    const [isHovered, setIsHovered] = useState(false);
+// image on the right
+export const FilmRight = ({ title, director, cast, genre, year, movie_image }: IFilm) => {
 
 
     return (
         <div className='FilmDivWrapper'>
 
             <div className='FilmTextContainer'>
-                <div className='FilmTextContainerTitle'>
-                    <h2>{title}</h2>
+                <div className='FilmTextContainerTitleRight'>
+                    <h3>{title}</h3>
                 </div>
                 <div className='FilmTextContainerYear'>
                     <p>{year}</p>
                 </div>
             </div>
 
-            {/* <p>{director}</p>
-            <p>{cast}</p>
-            <p>{genre}</p> */}
-
-            <div
-                className='FilmImage'
-                onMouseOver={() => setIsHovered(true)}
-                onMouseOut={() => setIsHovered(false)}
-            >
-
-                <MovieImage
-                    src={movie_image}
-                />
-                {
-                    isHovered &&
-                    <div className={`FilmImageText  ${isHovered ? 'show-text' : ''}`} >
-                        <h2>{title}</h2>
-                        <p>
-                            директор: {director}
-                            <br />
-                            <br />
-                            с участието на: {cast}
-                            <br />
-                            <br />
-                            жанр: {genre}
-                        </p>
-                    </div>
-                }
+            <div className='FilmImage'>
+                <HoverFigure>
+                    <img src={movie_image} />
+                    <figcaption>
+                        <StyledH4>{title}</StyledH4>
+                        <div>
+                            <StyledPar>директор: {director}</StyledPar>
+                            <StyledPar>с участието на: {cast}</StyledPar>
+                            <StyledPar>жанр: {genre}</StyledPar>
+                        </div>
+                    </figcaption>
+                </HoverFigure>
             </div>
-        </div>
+        </div >
     )
 }
 
-export default Film
+
+
+// image on the Left
+export const FilmLeft = ({ title, director, cast, genre, year, movie_image }: IFilm) => {
+
+
+    return (
+        <div className='FilmDivWrapper'>
+
+            <div className='FilmImage'>
+                <HoverFigure>
+                    <img src={movie_image} />
+                    <figcaption>
+                        <StyledH4>{title}</StyledH4>
+                        <div>
+                            <StyledPar>директор: {director}</StyledPar>
+                            <StyledPar>с участието на: {cast}</StyledPar>
+                            <StyledPar>жанр: {genre}</StyledPar>
+                        </div>
+                    </figcaption>
+                </HoverFigure>
+            </div>
+
+            <div className='FilmTextContainer'>
+                <div className='FilmTextContainerTitleLeft'>
+                    <h3>{title}</h3>
+                </div>
+                <div className='FilmTextContainerYear'>
+                    <p>{year}</p>
+                </div>
+            </div>
+
+
+        </div >
+    )
+}
--- front-end/src/components/Films/FilmsList.tsx
+++ front-end/src/components/Films/FilmsList.tsx
@@ -1,6 +1,6 @@
 
 import '../../styles/Films_styles/FilmsList.scss';
-import Film from './Film';
+import { FilmRight, FilmLeft } from './Film';
 
 import movie_image from '../../assets/moviehero.png';
 
@@ -9,8 +9,11 @@ function FilmsList() {
 
     return (
         <div className='FilmsListWrapper'>
-            <Film title={'не поглеждай нагоре'} director={'Кристофър Нолан'} cast={'Крисчън Бейл, Морган Фрийман, Алекса Деми'} genre={'научна фантастика'} year={'2020'} movie_image={movie_image}/>
-            <Film title={'не поглеждай нагоре'} director={'Кристофър Нолан'} cast={'Крисчън Бейл, Морган Фрийман, Алекса Деми'} genre={'научна фантастика'} year={'2020'} movie_image={movie_image}/>
+
+            <FilmRight title={'не поглеждай нагоре'} director={'Кристофър Нолан'} cast={'Крисчън Бейл, Морган Фрийман, Алекса Деми'} genre={'научна фантастика'} year={'2020'} movie_image={movie_image}/>
+            
+            <FilmLeft title={'не поглеждай нагоре'} director={'Кристофър Нолан'} cast={'Крисчън Бейл, Морган Фрийман, Алекса Деми'} genre={'научна фантастика'} year={'2020'} movie_image={movie_image}/>
+        
         </div>
     )
 }
